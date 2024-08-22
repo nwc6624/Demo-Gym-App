4faa6561715b0d833d46cfae91992bba20226171
Noah <nwc6624@gmail.com>
2024-08-22 15:57:53 -0600

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome to My React LOL Native App!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
